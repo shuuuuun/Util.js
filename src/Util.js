7448b65584a053f17de5068a1ecb4cbab81727e0
motoki-shun <motoki-shun@kayac.com>
2016-04-03 10:04:26 +0900

    オブジェクトverに一本化

--- src/Util.js
+++ src/Util.js
@@ -1,111 +1,128 @@
 (function(win, doc){
+  
   var $win = $(win);
   
-  function Util(){
-    this.getUserAgent();
-    this.getWinSize();
-  }
+  // prefix: 
+  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function(callback){ var id = window.setTimeout(callback,1000/60); return id; };
+  window.cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame || window.webkitCancelAnimationFrame || window.msCancelAnimationFrame || window.oCancelAnimationFrame || function(id){ window.clearTimeout(id); };
   
-  Util.prototype.bindOnResize = function(){
-    var that = this;
-    
-    $win.on("resize", that.throttle(function(){
-      that.getWinSize();
-    },500));
-  };
-  
-  Util.prototype.getWinSize = function(){
-    win.winW = Math.max( $win.width(), (win.innerWidth || 0) );
-    win.winH = Math.max( $win.height(), (win.innerHeight || 0) );
-  };
-  
-  Util.prototype.getRandomInt = function(min, max){
-    return Math.floor( Math.random() * (max - min + 1) ) + min;
-  };
-  
-  Util.prototype.throttle = function(fn, interval){
-    var isWaiting = false;
-    var exec = function(event) {
-      if (isWaiting) return;
-      isWaiting = true;
-      setTimeout(function() {
-        isWaiting = false;
-        fn(event);
-      }, interval);
-    };
-    return exec;
-  };
-  
-  Util.prototype.debounce = function(fn, interval){
-    var timer;
-    var exec = function(event) {
-      clearTimeout(timer);
-      timer = setTimeout(function() {
-        fn(event);
-      }, interval);
-    };
-    return exec;
-  };
-  
-  Util.prototype.async = function(fnList){
-    // fnList ... 第一引数にcallbackを取る関数の配列
-    (function exec(index){
-      if (!fnList[index]) return;
-      fnList[index](function(){
-        exec(index + 1);
-      });
-    })(0);
-  };
-  
-  Util.prototype.delay = function(time){ // asyncで使う用
-    return function(callback){
-      setTimeout(callback, time);
-    };
-  };
-  
-  Util.prototype.sleep = function(time){ // Deferred
-    return function(){
-      var dfd = $.Deferred();
-      setTimeout(function(){
-        dfd.resolve();
-      }, time);
-      return dfd.promise(); 
-    };
-  };
-  
-  Util.prototype.zeroPadding = function(num, len){
-    return (new Array(len).join("0") + num).slice(-len);
-  };
-  
-  Util.prototype.getQueryString = function(){
-    var result = {};
-    var search = win.location.search;
-    if (search.length > 1) {
-      var query = search.substring(1);
-      var parameters = query.split("&");
-      for(var i = 0; i < parameters.length; i++){
-        var element = parameters[i].split("=");
-        var paramName = decodeURIComponent(element[0]);
-        var paramValue = decodeURIComponent(element[1]);
-        result[paramName] = paramValue;
+  win.Util = {
+    TRANSITIONEND: "transitionend webkitTransitionEnd mozTransitionEnd msTransitionEnd oTransitionEnd",
+    ANIMATIONEND: "animationend webkitAnimationEnd mozAnimationEnd msAnimationEnd oAnimationEnd",
+    getWinSize: function(){
+      win.winW = Math.max( $win.width(), (win.innerWidth || 0) );
+      win.winH = Math.max( $win.height(), (win.innerHeight || 0) );
+    },
+    getRandomInt: function(min, max){
+      return Math.floor( Math.random() * (max - min + 1) ) + min;
+    },
+    throttle: function(fn, interval){
+      var isWaiting = false;
+      var exec = function(event) {
+        if (isWaiting) return;
+        isWaiting = true;
+        setTimeout(function() {
+          isWaiting = false;
+          fn(event);
+        }, interval);
+      };
+      return exec;
+    },
+    debounce: function(fn, interval){
+      var timer;
+      var exec = function(event) {
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+          fn(event);
+        }, interval);
+      };
+      return exec;
+    },
+    async: function(fnList){
+      // fnList ... 第一引数にcallbackを取る関数の配列
+      (function exec(index){
+        if (!fnList[index]) return;
+        fnList[index](function(){
+          exec(index + 1);
+        });
+      })(0);
+    },
+    delay: function(time){ // asyncで使う用
+      return function(callback){
+        setTimeout(callback, time);
+      };
+    },
+    sleep: function(time){ // Deferred
+      return function(){
+        var dfd = $.Deferred();
+        setTimeout(function(){
+          dfd.resolve();
+        }, time);
+        return dfd.promise(); 
+      };
+    },
+    zeroPadding: function(num, len){
+      return (new Array(len).join("0") + num).slice(-len);
+    },
+    getQueryString: function(){
+      var result = {};
+      var search = win.location.search;
+      if (search.length > 1) {
+        var query = search.substring(1);
+        var parameters = query.split("&");
+        for(var i = 0; i < parameters.length; i++){
+          var element = parameters[i].split("=");
+          var paramName = decodeURIComponent(element[0]);
+          var paramValue = decodeURIComponent(element[1]);
+          result[paramName] = paramValue;
+        }
       }
-    }
-    return result;
+      return result;
+    },
+    getUserAgent: function(){
+      Util.ua = {};
+      Util.ua.name = win.navigator.userAgent.toLowerCase();
+      Util.ua.isSP = /ipod|iphone|ipad|android/i.test(Util.ua.name);
+      Util.ua.isPC = !Util.ua.isSP;
+      Util.ua.isIOS = /ipod|iphone|ipad/i.test(Util.ua.name);
+      Util.ua.isAndroid = /android/.test(Util.ua.name);
+      Util.ua.isIE = /msie|trident/i.test(Util.ua.name);
+      Util.ua.isIE8 = /msie 8/.test(Util.ua.name);
+      Util.ua.isIE9 = /msie 9/.test(Util.ua.name);
+      Util.ua.isIE10 = /msie 10/.test(Util.ua.name);
+      Util.ua.isMac = /macintosh/.test(Util.ua.name);
+      Util.ua.isChrome = /chrome/.test(Util.ua.name);
+      Util.ua.isFirefox = /firefox/.test(Util.ua.name);
+      Util.ua.isSafari = /safari/.test(Util.ua.name);
+      Util.ua.isMacSafari = Util.ua.isSafari && Util.ua.isMac && !Util.ua.isChrome; // chromeのuaにもsafariの文字列がある
+      if (Util.ua.isSP) doc.body.className += " isSP";
+      if (Util.ua.isPC) doc.body.className += " isPC";
+      return Util.ua;
+    },
+    removeOtherDeviceElement: function(){
+      if (Util.ua.isSP) {
+        $('.onlypc').remove();
+      }
+      else {
+        $('.onlysp').remove();
+      }
+    },
+    setupInternalLinks: function(){
+      $('a[href^="#"]').on('click', function(){
+        var scrTop = document.documentElement.scrollTop || document.body.scrollTop || $win.scrollTop();
+        var speed = 2;
+        var anchor = $(this).attr('href');
+        var target = (anchor === '#top') ? 0 : $(anchor).offset().top;
+        var distance = Math.abs(scrTop - target);
+        var time = distance / speed;
+        
+        $('html, body').animate({
+            scrollTop: target,
+        }, time, 'swing');
+        
+        return false;
+      });
+    },
   };
-  
-  Util.prototype.getUserAgent = function(){
-    this.ua = {};
-    this.ua.name = win.navigator.userAgent.toLowerCase();
-    this.ua.isSP = /ipod|iphone|ipad|android/i.test(this.ua.name);
-    this.ua.isPC = !this.ua.isSP;
-    this.ua.isIOS = /ipod|iphone|ipad/i.test(this.ua.name);
-    this.ua.isAndroid = /android/.test(this.ua.name);
-    this.ua.isIE8 = /msie 8/.test(this.ua.name);
-    this.ua.isIE9 = /msie 9/.test(this.ua.name);
-    if (this.ua.isSP) doc.body.className += " isSP";
-    if (this.ua.isPC) doc.body.className += " isPC";
-  };
-  
-  win.Util = Util;
   
 })(this, document);
